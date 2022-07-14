f5d1228bdcc40681e82f3b0673349da9e8014824
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2022-07-14 15:39:51 +0300

    try to revert 1 commit

--- Quid_pro_Quo/ClientApp/src/app/app.module.ts
+++ Quid_pro_Quo/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-import { GoogleMapsModule } from '@angular/google-maps';
+//import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -49,7 +49,7 @@ const appRoutes: Routes = [
     UserPageComponent,
   ],
   imports: [
-    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes), GoogleMapsModule
+    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes), //GoogleMapsModule
   ],
   providers: [ AuthorizationService, RequestService ],
   bootstrap: [ AppComponent ]
