cdfebe9ef8b715b57f1a9cfedc0ff6708e8be1eb
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2022-07-17 15:25:55 +0300

    fix: all

--- Quid_pro_Quo/ClientApp/src/app/app.module.ts
+++ Quid_pro_Quo/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-//import { GoogleMapsModule } from '@angular/google-maps';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -49,9 +49,9 @@ const appRoutes: Routes = [
     UserPageComponent,
   ],
   imports: [
-    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes), //GoogleMapsModule
+    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes), GoogleMapsModule
   ],
-  providers: [ AuthorizationService, RequestService ],
-  bootstrap: [ AppComponent ]
+  providers: [AuthorizationService, RequestService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
