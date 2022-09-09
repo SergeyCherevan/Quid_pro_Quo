68a335960c91674267e71a7113a4d6040be2613f
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2022-09-09 09:45:10 +0300

    add: http sender on c++ using libcurl

---
+++ IoT/HttpSender/HttpSender/HttpSender.cpp
@@ -0,0 +1,30 @@
+﻿// HttpSender.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define CURL_STATICLIB
+
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+    CURL* curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://localhost:5001/");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+    }
+
+    return 0;
+}
